1b23fc71b8847dd7ea5f4d8cfc4d8cbb00a7f729
RogerioHenrique1403 <rogerioh0911@gmail.com>
2025-11-25 15:02:18 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from pessoa import pessoa
+
+p1 = pessoa('Luiz', 29)
+p1.comer('Maçã')
+p1.falar('POO')
+p1.parar_comer()
+p1.falar('POO')
+p1.comer('alimento')
+p1.parar_falar()
+p1.falar('assunto')
+print(p1.get_ano_nascimento())
